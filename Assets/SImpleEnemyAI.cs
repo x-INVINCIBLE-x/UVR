2d0f62753c5813236b68db838e69fbf96f0d09b5
Devil-Bringer2005 <gauthamsuresh36@gmail.com>
2025-05-14 20:31:43 +0530

    Archery Minigame 2

---
+++ Assets/SImpleEnemyAI.cs
@@ -0,0 +1,143 @@
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SImpleEnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform Player;
+
+    public GameObject bullet;
+
+    public LayerMask WhatIsground, WhatIsPlayer;
+
+
+    // Straight to Destination
+    public bool goToDestination = false;
+
+    // Patrolling 
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkpointRange;
+
+    public Transform Destination;
+
+    // Attacking 
+    public float attackCooldownTime;
+    public bool hasAttacked;
+
+    // States
+
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        Player = GameObject.Find("Cube").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, WhatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, WhatIsPlayer);
+
+        if(!playerInSightRange && !playerInAttackRange && goToDestination)
+        {
+            GoToDestination();
+        }
+
+        if(!playerInSightRange && !playerInAttackRange && !goToDestination)
+        {
+            Patrolling();
+        }
+
+        if(playerInSightRange && !playerInAttackRange)
+        {
+            ChasePlayer();
+        }
+
+        if (playerInSightRange && playerInAttackRange)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if(distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+
+    private void GoToDestination()
+    {
+        agent.SetDestination(Destination.position);
+    }
+    private void SearchWalkPoint()
+    {
+        float randomz = Random.Range(-walkpointRange, walkpointRange);
+        float randomx = Random.Range(-walkpointRange,walkpointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomx,transform.position.y ,transform.position.z + randomz);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, WhatIsground))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(Player.position);
+    }
+
+    private void AttackPlayer()
+    {   
+        // Stops the agent from moving
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(Player);
+
+        if (!hasAttacked)
+        {
+            Rigidbody rb = Instantiate(bullet, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse); 
+            //rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            
+
+
+            hasAttacked = true;
+            Invoke(nameof(ResetAttack), attackCooldownTime);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        hasAttacked = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
