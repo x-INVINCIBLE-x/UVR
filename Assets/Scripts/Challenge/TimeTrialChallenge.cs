e461d4e6be9a72269460e4c41fd9fcd80c5b3a77
x-INVINCIBLE-x <shubham170905@gmail.com>
2025-06-29 09:38:19 +0530

    Fix Errors

--- Assets/Scripts/Challenge/TimeTrialChallenge.cs
+++ Assets/Scripts/Challenge/TimeTrialChallenge.cs
@@ -1,15 +1,37 @@
+using Autodesk.Fbx;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+
+public enum QuestTarget
+{
+    Melee,
+    Ranged,
+    Crystal,
+    Statue
+}
 
 public class TimeTrialChallenge : Challenge
 {
+    [SerializeField] private int targetAmount;
+    private List<int> possibleTargets = new();
+
+    private QuestTarget currentTarget;
+    
 
     public override void InitializeChallenge()
     {
+        if (possibleTargets.Count != 0) { return; }
 
+        ResetTargets();
     }
 
     public override void StartChallenge()
     {
+        int targetIndex = UnityEngine.Random.Range(0, possibleTargets.Count);
+        currentTarget = (QuestTarget)possibleTargets[targetIndex];
+
+        possibleTargets.RemoveAt(targetIndex);
 
     }
 
@@ -21,5 +43,13 @@ public class TimeTrialChallenge : Challenge
     public override void ChallengeFailed()
     {
 
+    }
+
+    private void ResetTargets()
+    {
+        for (int i = 0; i < Enum.GetValues(typeof(Target)).Length; i++)
+        {
+            possibleTargets.Add(i);
+        }
     }
 }
