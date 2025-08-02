cec0cedbbad99ad64f981834b16c480c4883a0a9
Devil-Bringer2005 <gauthamsuresh36@gmail.com>
2025-08-02 21:54:37 +0530

    Full screen shader and End Block

--- Assets/Scripts/FullscreenEffectController.cs
+++ Assets/Scripts/FullscreenEffectController.cs
@@ -13,9 +13,14 @@ public class FullscreenEffectController : MonoBehaviour
 
     [Header("References")]
 
-    [SerializeField] private ScriptableRendererFeature fullScreenEffect;
-    [SerializeField] private List<Material> efffectMaterials;
-
+    [SerializeField] private FullScreenPassRendererFeature fullScreenEffect;
+    [SerializeField] private Material FireEffect;
+    [SerializeField] private Material IceEffect;
+    [SerializeField] private Material ElectricEffect;
+    [SerializeField] private Material DendroEffect;
+    [SerializeField] private Material HolyEffect;
+    [SerializeField] private Material DarkEffect;
+    
     // Shader Variables (Property) 
     private static readonly int effectIntensity = Shader.PropertyToID("_Vignette_Intensity");
     private static readonly int effectPower = Shader.PropertyToID("_Vignette_Power");
@@ -23,6 +28,13 @@ public class FullscreenEffectController : MonoBehaviour
     // Default value of the effect material
     private int defaulteffectIntensity; 
     private int defaulteffectPower;
+
+    // Coroutines 
+    private Coroutine startCoroutine;
+    private Coroutine endCoroutine;
+
+    // Material Reference
+    private Material effectType;
 
     //[SerializeField] private Color color;
     //[SerializeField] private float intensity = 8f;
@@ -35,8 +47,72 @@ public class FullscreenEffectController : MonoBehaviour
         defaulteffectIntensity = effectIntensity;
         defaulteffectPower = effectPower;
     }
-    private IEnumerator StartFullscreenEffect()
+
+    private void Start()
     {
+        fullScreenEffect.SetActive(false);
+    }
+
+    public void ActivateFullscreenEffect(AilmentType ailmentType)
+    {
+
+        if(ailmentType == AilmentType.Ignis)
+        {
+            effectType = FireEffect;
+        }
+        else if(ailmentType == AilmentType.Gaia)
+        {
+            effectType = DendroEffect;
+        }
+        else if(ailmentType == AilmentType.Blitz)
+        {
+            effectType = ElectricEffect;
+        }
+        else if(ailmentType == AilmentType.Frost)
+        {
+            effectType = IceEffect;
+        }
+        else if(ailmentType == AilmentType.Hex)
+        {
+            effectType = DarkEffect;
+        }
+        else if(ailmentType == AilmentType.Radiance)
+        {
+            effectType = HolyEffect;
+        }
+        
+        if(startCoroutine != null)
+        {
+            StopCoroutine(startCoroutine);
+        }
+        startCoroutine = StartCoroutine(StartFullscreenEffect(effectType));
+    }
+
+    public void DeactivateFullscreenEffect()
+    {
+        endCoroutine = StartCoroutine(EndFullscreenEffect(effectType));
+    }
+
+    private IEnumerator StartFullscreenEffect(Material effectMaterial)
+    {   
+
+        fullScreenEffect.SetActive(true);
+        fullScreenEffect.passMaterial = effectMaterial;
+        startCoroutine = null;
         yield return null;
+    }
+
+    private IEnumerator EndFullscreenEffect(Material effectMaterial)
+    {
+        fullScreenEffect.SetActive(false);
+        fullScreenEffect.passMaterial = effectMaterial;
+        endCoroutine = null;
+        yield return null;
+    }
+
+    private void OnDisable()
+    {
+        effectType.SetFloat(effectIntensity, defaulteffectIntensity);
+        effectType.SetFloat(effectPower, defaulteffectPower);
     }
 }
