6059e18346a4d84db102486d5e76af8328e88f5a
Devil-Bringer2005 <gauthamsuresh36@gmail.com>
2025-08-29 22:09:43 +0530

    Moving trap and enemy sound added

---
+++ Assets/Scripts/Traps/Moving traps.cs
@@ -0,0 +1,44 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class Movingtraps : MonoBehaviour
+{
+    [Space]
+    [Header("Moving trap Setup")]
+    [Space]
+    public Vector3 movetoOffset;
+    private Vector3 startPosition;
+    [SerializeField] private float moveDuration;
+
+    [Space]
+    [Header("Ease Type")]
+    [Space]
+    [SerializeField] Ease ease; // ease type 
+
+    [Space]
+    [Header("loopType")]
+    [Space]
+    [SerializeField] LoopType loopType; // Looping type
+
+
+    private void Start()
+    {
+        startPosition = transform.localPosition;
+        MovingTrapActivate();
+    }
+
+    public void MovingTrapActivate()
+    {
+        //Debug.Log("Moving Trap Activated");
+        // Add logic for Moving traps
+        Debug.Log($"Moving Trap Activated with ease: {ease}");
+        transform.DOLocalMove(startPosition + movetoOffset, moveDuration).SetEase(ease).SetLoops(-1, loopType);
+        RotateTrap();
+
+    }
+
+    public void RotateTrap()
+    {
+        transform.DORotate(new Vector3(0, 360f, 0), 1.5f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetRelative().SetEase(Ease.Linear);
+    }
+}
