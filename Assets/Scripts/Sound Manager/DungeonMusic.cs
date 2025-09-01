cc6ee4be648f09de392bc06e9ca1d71e7dd6de0c
Devil-Bringer2005 <gauthamsuresh36@gmail.com>
2025-09-02 01:50:52 +0530

    Dungeon Music

--- Assets/Scripts/Sound Manager/DungeonMusic.cs
+++ Assets/Scripts/Sound Manager/DungeonMusic.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 
 public class DungeonMusic : MonoBehaviour
 {   
-    public AudioClip dungeonTheme;
-    public AudioClip battleTheme;
+    public AudioClip[] dungeonTheme;
+    public AudioClip[] battleTheme;
+    private int indexDungeon;
+    private int indexBattle;
 
     [SerializeField] private float transitionTime = 2f;
     private bool inCombat = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {   
-        AudioManager.Instance.PlayMusic(dungeonTheme);
+    {
+        RandomMusic();
+        //Debug.Log(dungeonTheme[indexDungeon]);
+        //Debug.Log(battleTheme[indexBattle]);
+
+        AudioManager.Instance.PlayMusic(dungeonTheme[indexDungeon]);
         EnemyEventManager.Instance.OnEnemySeePlayer  += OnEngageCombat;
         EnemyEventManager.Instance.OnEnemyLostPlayer += OnDisengageCombat;
         EnemyEventManager.Instance.OnEnemyDeath += OnDisengageCombat; // Also disengage combat when all enemies are dead
     }
 
     private void OnDestroy()
-    {
+    {   
         EnemyEventManager.Instance.OnEnemySeePlayer -= OnEngageCombat;
         EnemyEventManager.Instance.OnEnemyLostPlayer -= OnDisengageCombat;
         EnemyEventManager.Instance.OnEnemyDeath -= OnDisengageCombat; // Unsubscribe to avoid memory leaks
@@ -29,19 +35,35 @@ public class DungeonMusic : MonoBehaviour
 
     private void OnEngageCombat(int enemyId)
     {
+        //RandomMusic();
+        //Debug.Log(dungeonTheme[indexDungeon]);
+        //Debug.Log(battleTheme[indexBattle]);
+
         if (!inCombat)
         {
             inCombat = true;
-            AudioManager.Instance.PlayMusicWithCrossFade(battleTheme, transitionTime);
+            AudioManager.Instance.PlayMusicWithCrossFade(battleTheme[indexBattle], transitionTime);
         }
     }
     
     private void OnDisengageCombat(int enemyID)
     {
-        if(inCombat && EnemyEventManager.Instance.ActiveEnemies.Count == 0)
+        //RandomMusic();
+        //Debug.Log(dungeonTheme[indexDungeon]);
+        //Debug.Log(battleTheme[indexBattle]);
+
+        if (inCombat && EnemyEventManager.Instance.ActiveEnemies.Count == 0)
         {
             inCombat = false;
-            AudioManager.Instance.PlayMusicWithCrossFade(dungeonTheme, transitionTime);
+            AudioManager.Instance.PlayMusicWithCrossFade(dungeonTheme[indexDungeon], transitionTime);
         }
     }
+
+    private void RandomMusic() 
+    {
+        indexDungeon = UnityEngine.Random.Range(0, dungeonTheme.Length);
+        indexBattle = UnityEngine.Random.Range(0,battleTheme.Length);
+    }
+
+    
 }
