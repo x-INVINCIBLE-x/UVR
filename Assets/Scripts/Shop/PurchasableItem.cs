15244415baaa0ad942d3401ab7c15d6b960fe8b0
Devil-Bringer2005 <gauthamsuresh36@gmail.com>
2025-09-21 20:09:07 +0530

    fixes

--- Assets/Scripts/Shop/PurchasableItem.cs
+++ Assets/Scripts/Shop/PurchasableItem.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
@@ -6,125 +6,64 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 public class PurchasableItem : MonoBehaviour
 {
     [Header("References")]
-    [Space]
     public ItemData itemData;
+    [Tooltip("Mark true if this item is sold in the shop.")]
     public bool isShopItem = false;
 
     private XRGrabInteractable grabInteractable;
     [SerializeField] private bool hasPurchased = false;
 
-    [Header("Visuals Settings")]
-
-    [Space]
-
-    public MeshRenderer[] meshes; // Mesh renderer component of all the meshes in 3d model 
-    public Material[] defaultMaterial; // default material of all mesh renderers
-
-    [Space]
-
-    public Material AffordableMaterial; // Material for items that we can buy
-    public Material UnaffordableMaterial; // Material for item that we cannot buy
-
-    [Space]
-
-    private int layerMask; 
+    private int playerLayerMask;
 
     private void Awake()
-    {   
-        if(!isShopItem)
-            hasPurchased = true;
-
+    {
         grabInteractable = GetComponent<XRGrabInteractable>();
+
+        if (!isShopItem)
+        {
+            // Normal item → already considered purchased
+            hasPurchased = true;
+        }
+
+        // Subscribe to events
         grabInteractable.selectEntered.AddListener(OnGrabAttempt);
-        grabInteractable.hoverEntered.AddListener(OnHoverAttempt);
-        grabInteractable.hoverExited.AddListener(OnHoverExit);
-        layerMask = LayerMask.GetMask("Player");
 
-        // Highlighting 
-        meshes = GetComponentsInChildren<MeshRenderer>();
-        defaultMaterial = new Material[meshes.Length];
-
-        for (int i = 0; i < meshes.Length; i++)
-        {
-            defaultMaterial[i] = meshes[i].material;
-            //Debug.Log("Mat" + meshes[i].material.name);
-        }
-    }
-
-
-    private void OnHoverAttempt(HoverEnterEventArgs arg0)
-    {   
-        if(AffordableMaterial != null && UnaffordableMaterial != null && hasPurchased == false )
-        {
-            if (CurrencyManager.Instance.Gold >= itemData.ItemCost)
-            {
-                // Change to affordable material
-                foreach (MeshRenderer renderer in meshes)
-                {
-                    renderer.material = AffordableMaterial;
-                }
-
-            }
-            else if (CurrencyManager.Instance.Gold < itemData.ItemCost)
-            {
-                // Change to Unaffordable material
-                foreach (MeshRenderer renderer in meshes)
-                {
-                    renderer.material = UnaffordableMaterial;
-                }
-
-            }
-        }
-    
-    }
-    private void OnHoverExit(HoverExitEventArgs arg0)
-    {
-        Invoke(nameof(ResetMaterial), 0.5f);// Reset to default material  
-    }
-
-    private void OnDestroy()
-    {
-        grabInteractable.selectEntered.RemoveListener(OnGrabAttempt);
-        grabInteractable.hoverEntered.RemoveListener(OnHoverAttempt);
-        grabInteractable.hoverExited.RemoveListener(OnHoverExit);
-
+        // Player layer for restriction
+        playerLayerMask = LayerMask.GetMask("Player");
     }
 
     private void OnGrabAttempt(SelectEnterEventArgs args)
     {
-        if ((layerMask & (1 << args.interactorObject.transform.gameObject.layer)) == 0)
+        // Check if grabber is player
+        if ((playerLayerMask & (1 << args.interactorObject.transform.gameObject.layer)) == 0)
         {
-            Debug.Log(args.interactorObject.transform.gameObject.name);
-            Debug.Log("Block");
+            Debug.Log("Grab blocked: not player");
             return;
         }
 
-        if (!isShopItem)
-            return;
+        // Normal item → always grabbable
+        if (!isShopItem) return;
 
-        if (hasPurchased)
-            return;
+        // Already purchased → grabbable
+        if (hasPurchased) return;
 
         var moneyManager = CurrencyManager.Instance;
-
         if (moneyManager == null)
-        {   
-            Debug.Log("Money Manager is Missing");
+        {
+            Debug.LogWarning("CurrencyManager is missing!");
             CancelGrab(args);
             return;
         }
 
         if (moneyManager.Gold >= itemData.ItemCost)
-        {   
+        {
             moneyManager.SpendGold(itemData.ItemCost);
             hasPurchased = true;
             Debug.Log($"{itemData.Name} purchased for {itemData.ItemCost}");
         }
-
         else
         {
-            
-            Debug.Log($" not enough money to buy {itemData.Name}");
+            Debug.Log($"Not enough money to buy {itemData.Name}");
             CancelGrab(args);
         }
     }
@@ -137,11 +76,9 @@ public class PurchasableItem : MonoBehaviour
         }
     }
 
-    private void ResetMaterial()
+    private void OnDestroy()
     {
-        for (int i = 0; i < meshes.Length; i++)
-        {
-            meshes[i].material = defaultMaterial[i];
-        }
+        // Unsubscribe
+        grabInteractable.selectEntered.RemoveListener(OnGrabAttempt);
     }
 }
